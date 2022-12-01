b0462c44c5b9fabf1498ead08e7513931893c1e7
Zabicki <krzysztof.zabicki7@gmail.com>
2022-12-02 00:17:21 +0100

    Add day 1 solution

---
+++ src/main/java/CalorieCounter.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CalorieCounter {
+
+    private static final String INPUT_PATH = "src/main/resources/input.txt";
+
+    public static void main(String[] args) throws IOException {
+        CalorieCounter counter = new CalorieCounter();
+
+        // first task
+        System.out.println(counter.sumTopCalories(1));
+        // second task
+        System.out.println(counter.sumTopCalories(3));
+    }
+
+    public int sumTopCalories(int n) throws IOException {
+        List<Integer> calorieSums = new LinkedList<>();
+        calorieSums.add(0);
+        int elf = 0;
+
+        for (String line : Files.readAllLines(Paths.get(INPUT_PATH))) {
+            if ("".equals(line)) {
+                calorieSums.add(0);
+                ++elf;
+            } else {
+                calorieSums.set(elf, calorieSums.get(elf) + Integer.parseInt(line));
+            }
+        }
+        return calorieSums.stream()
+                .sorted(Comparator.reverseOrder())
+                .limit(n)
+                .reduce(Integer::sum).orElse(0);
+    }
+}
